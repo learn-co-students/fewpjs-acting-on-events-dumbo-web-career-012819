c7e571509c10d1835e6eaec224afa35a9e6e19c6
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-11 19:48:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 // Your code here
 
+var dodger = document.getElementById('dodger');
+
 function moveDodgerLeft() {
-  document.getElementById('dodger').style.left = "100px";
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
 
 function moveDodgerRight() {
-  document.getElementById('dodger').style.left = "200px";
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
 }
+
+function moveDodgerTop() {
+  var topNumbers = dodger.style.top.replace('px', '');
+  var top = parseInt(topNumbers, 10);
+
+  if (top >= 0) {
+    dodger.style.top = `${top - 1}px`;
+  }
+}
+
+function moveDodgerBottom() {
+  var bottomNumbers = dodger.style.bottom.replace('px', '');
+  var bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom >= 0) {
+    dodger.style.bottom = `${bottom - 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  } else if (e.which === 39) {
+    moveDodgerRight();
+  } else if (e.which === 38) {
+    moveDodgerTop();
+  } else if (e.which === 40) {
+    moveDodgerBottom();
+  }
+})
